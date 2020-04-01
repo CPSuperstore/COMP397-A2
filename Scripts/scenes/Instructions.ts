269079b077a765d6a8e5b67535c1d6b3b0a75433
Christopher <christopher.rocks@bell.net>
2020-04-01 15:31:36 -0400

    Added text to the instructions page

--- Scripts/scenes/Instructions.ts
+++ Scripts/scenes/Instructions.ts
@@ -4,6 +4,7 @@ module scenes
     {
         // PRIVATE INSTANCE MEMBERS
         titleLabel:objects.Label;
+        body:objects.Label;
         endButton:objects.Button;
 
         // PUBLIC PROPERTIES
@@ -13,8 +14,25 @@ module scenes
         {
             super();
 
+            let text = `
+            Floofie's Adventure!
+            
+            Read the story in the panel on the right side of the screen
+            which has conveniently labeled "Story"
+
+            In the first level, use W and D to move up and down
+            Shoot enemies to kill them
+            Collect cat treats to regain some health
+
+            In the seccond level, use the space bar to pick the lock
+            Push each lock pin to break the lock and rescue mousey
+
+            Starting My Girlfriend's Cat as Queen Floofie!  
+            `;
+
             this.titleLabel = new objects.Label("Instructions", "80px","Arial", "Black", 320, 50, true);
-            this.endButton = new objects.Button("./Assets/images/gui/back.png", 320, 450, true);
+            this.body = new objects.Label(text.replace("\t", ""), "20px","Arial", "Black", 320, 100, true);
+            this.endButton = new objects.Button("./Assets/images/gui/back.png", 320, 425, true);
 
             this.Start();
         }
@@ -33,6 +51,7 @@ module scenes
         public Main(): void {
             this.addChild(this.titleLabel);
             this.addChild(this.endButton);
+            this.addChild(this.body)
     
             this.endButton.on("click", function() {
                config.Game.SCENE_STATE = scenes.State.START;
